f568dd1fae6bc9b1d0ae3cc4a2b15f4d6d43eb1e
Max Edell <maxed@adobe.com>
2021-06-01 12:44:52 -0700

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+### firefox-bug
+
+This repo is a reproducible case in Firefox where a [Link header](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Link) containing a list of links is squashed in the cache to only the serve the last entry when the if-match hits and server returns 304.
+
+### Environment
+- Firefox v88.0.1 (64-bit)
+- macOS 11.2.3 (and others)
+
+### Run it
+```sh
+yarn start
+```
+
+Open Firefox to http://localhost:2337
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const host = process.env.HOST || '127.0.0.1';
+const port = process.env.PORT || 2337;
+const etag = 'some-etag';
+const links = [
+    '<ref>; param1=value1;',
+    '<ref2>; param2=value2',
+    '<ref3>; param1=value1;'
+];
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Expose-Headers', 'ETag,Link');
+    next();
+});
+
+app.use('/', express.static('public'));
+
+app.options('/test', (req, res) => {
+    res.setHeader('Access-Control-Request-Method', 'HEAD');
+    return res.status(200).send();
+});
+
+app.head('/test', (req, res) => {
+    if(req.headers['if-match'] === etag) {
+        return res.status(304).send();
+    }
+
+    res.setHeader('etag', etag);
+    res.setHeader('link', links);
+    return res.status(200).send();
+});
+
+app.listen(port, host, () => {
+    console.info(`Started server at http://${host}:${port}`);
+});
